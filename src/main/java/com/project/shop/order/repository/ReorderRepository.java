5a762f6544cf884e435209d936f072d0a8d30c78
chandanacsee99 <67309979+chandanacsee99@users.noreply.github.com>
2021-09-12 17:31:21 +0530

    Delete src directory

--- src/main/java/com/project/shop/order/repository/ReorderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.shop.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.shop.order.entity.Order;
-
-public interface ReorderRepository extends JpaRepository<Order, String>{
-	Order findByOrderid(String orderid);
-}
